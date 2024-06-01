a66a19b02a68dc4fcae80993db1804afc0240637
LuigiCaglio <83336089+LuigiCaglio@users.noreply.github.com>
2024-06-01 22:18:02 +0200

    Update ops_2story_1bay.py - corrected code

--- OpenSees_models/ops_2story_1bay.py
+++ OpenSees_models/ops_2story_1bay.py
@@ -7,379 +7,80 @@ Created on Tue Feb 27 18:30:21 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
+import openseespy.opensees as ops
 import opsvis
-from ops_2story_1bay import define_model_2story
-import corner
-from scipy.stats import truncnorm
 
 
+def define_model_2story(mass_1fact=1,mass_2fact=1,
+                         stiff1_fact=1,stiff2_fact=1,):
 
-
-true_params = [1.1,0.9,0.84,1.05]
-
-
-# true_params = [1.05,0.94,0.96,1.02]
-
-freq,mode1,mode2 = define_model_2story(mass_1fact=true_params[0],
-                                        mass_2fact=true_params[1],
-                                        stiff1_fact=true_params[2],
-                                        stiff2_fact=true_params[3],)
-
-opsvis.plot_model()
-
-opsvis.plot_mode_shape(1,)
-opsvis.plot_mode_shape(2,)
-
-print("periods :",1/freq)
-
-
-
-#%% define probability distributions
-
-def MAC(phi_1,phi_2):
-    MAC = np.dot(phi_1,phi_2)**2/ (phi_1.dot(phi_1)*phi_2.dot(phi_2))
-    return MAC
-
-def gaussian_distribution(x,mu, sigma):
-    y = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
-    return y
-
-
-
-def truncated_gaussian_pdf(x, mu, sigma, a, b):
-    x = np.array(x).reshape(-1)
-    # Calculate the truncated normal distribution bounds relative to the mean and std
-    a, b = (a - mu) / sigma, (b - mu) / sigma
+    ops.wipe()
+    ops.model('basic', '-ndm', 2, '-ndf', 3)
     
-    tn_dist = truncnorm(a, b, loc=mu, scale=sigma)
+    L = 5
+    H = 3
+    mass1 = 5e3 /L * mass_1fact
+    mass2 = 5e3 /L * mass_2fact
     
-    # Calculate the PDF for the given x
-    pdf = tn_dist.pdf(x)
+    #HEB 500
+    A_ele = 238.60 *0.01**2
+    E_mod = 210e9
+    Iz = 107180 *0.01**4
     
-    return pdf
-
-def truncated_uniform_pdf(x, mu, sigma, a, b):
-    x = np.array(x).reshape(-1)
+    mass_column = A_ele * 7850
     
-    constant_value = 1/(b-a)
-    pdf = np.where((x >= a) & (x <= b), constant_value, 0)
+    ops.node(10, *[0,0]); ops.fix(10, *[1,1,1,])
+    ops.node(11, *[L,0]); ops.fix(11, *[1,1,1,])
     
+    ops.node(20, *[0,H]) 
+    ops.node(21, *[L,H])
     
-    return pdf
-
-
-
-plt.rcParams.update({'font.size': 16})  # Adjust the 16 to your desired font size
-
-def plot_distribution(distribution_function,params,title="", limits_plot = [0.85,1.15]):
-    # Generate data for the Gaussian distribution
-    mu,sigma = params[0],params[1]
-    x = np.linspace(mu*limits_plot[0], mu*limits_plot[1], 1000)
-    y = distribution_function(x,*params)
-    # Plot the distribution
-    plt.figure(figsize=(10, 6))
-    plt.plot(x, y, label=f'μ={mu:.2f}, σ={sigma:.2f}')
-    plt.title(title)
-    plt.xlabel('freq (Hz)')
-    plt.ylabel('Probability Density')
-    # plt.legend()
-    plt.grid(True)
-    plt.show()
+    ops.node(30, *[0,2*H]) 
+    ops.node(31, *[L,2*H])
     
+    ops.geomTransf("Linear",123)
+    
+    #columns 1st floor
+    ops.element('elasticBeamColumn', 1020, *[10,20], A_ele, stiff1_fact*E_mod, Iz, 123, '-mass', mass_column, '-cMass')
+    ops.element('elasticBeamColumn', 1121, *[11,21], A_ele, stiff1_fact*E_mod, Iz, 123, '-mass', mass_column, '-cMass')
+    
+    #columns 2nd floor
+    ops.element('elasticBeamColumn', 2030, *[20,30], A_ele, stiff2_fact*E_mod, Iz, 123, '-mass', mass_column, '-cMass')
+    ops.element('elasticBeamColumn', 2131, *[21,31], A_ele, stiff2_fact*E_mod, Iz, 123, '-mass', mass_column, '-cMass')
+    
+    #beams
+    ops.element('elasticBeamColumn', 2021, *[20,21], A_ele, E_mod, Iz, 123, '-mass', mass1, '-cMass')
+    ops.element('elasticBeamColumn', 3031, *[30,31], A_ele, E_mod, Iz, 123, '-mass', mass2, '-cMass')
     
 
-#define standard deviations frequency models
-sigma_f_i_normalized = 0.01
-
-sigma_f1 = sigma_f_i_normalized *freq[0]
-sigma_f2 = sigma_f_i_normalized *freq[1]
-
-
-
-sigma_prior = 0.5
-prior_upper_limit = 1.3
-prior_lower_limit = 0.7
-
-
-
-plot_distribution(gaussian_distribution,[freq[0], sigma_f1],"Frequency 1",[0.95,1.05])
-plot_distribution(gaussian_distribution,[freq[1], sigma_f2],"Frequency 2",[0.95,1.05])
-
-
-plot_distribution(truncated_uniform_pdf,[1,sigma_prior ,
-                                         prior_lower_limit,prior_upper_limit],
-                                        "Parameter prior - $P(m_1)$ and $P(m_2) $",[0.6,1.4])
-
-
-#%%
-fig, (ax1,ax2) = plt.subplots(1,2,figsize=(10, 6))
-
-limits_plot = [0.85,1.15]
-mu,sigma = freq[0], sigma_f1
-x = np.linspace(mu*limits_plot[0], mu*limits_plot[1], 1000)
-y = gaussian_distribution(x,*[freq[0], sigma_f1])
-# Plot the distribution
-
-ax1.plot(x, y, label=f'μ={mu:.2f}\n σ={sigma:.2f}')
-ax1.set_title("Frequency 1")
-ax1.set_xlabel('freq (Hz)')
-ax1.set_ylabel('Probability Density')
-
-limits_plot = [0.90,1.10]
-mu,sigma = freq[1], sigma_f2
-x = np.linspace(mu*limits_plot[0], mu*limits_plot[1], 1000)
-y = gaussian_distribution(x,*[freq[1], sigma_f2])
-# Plot the distribution
-
-ax2.plot(x, y, label=f'μ={mu:.2f}\n σ={sigma:.2f}')
-ax2.set_title("Frequency 2")
-ax2.set_xlabel('freq (Hz)')
-
-ax1.grid(True)
-ax2.grid(True)
-ax1.legend(loc='upper right',fontsize=12)
-ax2.legend(loc='upper right',fontsize=12)
-plt.show()
-
     
+    eigs = ops.eigen(6)
+
+    freq = np.array(eigs)**.5 / (2*np.pi)
+    periods = 1/freq
+
+    mode1 = []
+    mode1.append(ops.nodeEigenvector(21, 1,1))
+    mode1.append(ops.nodeEigenvector(31, 1,1))
     
-
-# %% define likelihood function - prior - unnormalized posterior
-
-def likelihood_function(params):
+    mode2 = []
+    mode2.append(ops.nodeEigenvector(21, 2,1))
+    mode2.append(ops.nodeEigenvector(31, 2,1))
     
-    freq_i,mode1_i,mode2_i = define_model_2story(mass_1fact=params[0],
-                                               mass_2fact=params[1],
-                                               # stiff1_fact=params[2],
-                                               # stiff2_fact=params[3]),
-                                               stiff1_fact=true_params[2],
-                                               stiff2_fact=true_params[3],)
+    return freq,np.array(mode1),np.array(mode2)
+
+if __name__ == '__main__':
+    true_params = [1.1,0.9,0.84,1.05]
+    print("true parameters: ", true_params)
     
-    likelihood_f_1 = gaussian_distribution(freq_i[0],freq[0], sigma_f1)
-    likelihood_f_2 = gaussian_distribution(freq_i[1],freq[1], sigma_f2)
+    freq,mode1,mode2 = define_model_2story(mass_1fact=true_params[0],
+                                            mass_2fact=true_params[1],
+                                            stiff1_fact=true_params[2],
+                                            stiff2_fact=true_params[3],)
     
-    MAC1 = MAC(mode1_i,mode1)
-    MAC2 = MAC(mode2_i,mode2)
-        
+    opsvis.plot_model()
     
-    likelihood_all = likelihood_f_1*likelihood_f_2
+    opsvis.plot_mode_shape(1,)
+    opsvis.plot_mode_shape(2,)
     
-    return likelihood_all, [freq_i,[MAC1,MAC2]]
-
-
-
-def prior(params):
-    
-    # prior_m1 = truncated_gaussian_pdf(params[0], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    # prior_m2 = truncated_gaussian_pdf(params[1], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    # prior_k1 = truncated_gaussian_pdf(params[2], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    # prior_k2 = truncated_gaussian_pdf(params[3], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    prior_m1 = truncated_uniform_pdf(params[0], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    prior_m2 = truncated_uniform_pdf(params[1], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    prior_k1 = truncated_uniform_pdf(params[2], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-    prior_k2 = truncated_uniform_pdf(params[3], mu=1, sigma=sigma_prior, a=prior_lower_limit, b=prior_upper_limit)[0]
-        
-    return prior_m1*prior_m2*prior_k1*prior_k2
-
-
-
-
-
-def unnormalized_posterior(params):
-    likelihood, freq_i = likelihood_function(params)
-    return prior(params)*likelihood,  freq_i
-
-
-#%% Bayesian inference - Markov Chain Monte Carlo
-
-
-
-std_step = 0.1
-
-def sampling_algorigm(theta_prev):
-    proposal = np.random.normal(theta_prev, std_step)
-    
-    return proposal
-    # return np.clip(proposal,prior_lower_limit,prior_upper_limit)
-    
-
-
-
-def metropolis_sampling(theta_prev,nll_prev):
-    theta_proposal = sampling_algorigm(theta_prev)
-    # nll_proposal = evaluate_likelihood(fact_proposal)
-    likelihood_proposal,freq_i = unnormalized_posterior(theta_proposal)
-    
-    u = np.random.rand()
-    
-    alpha_i = likelihood_proposal/nll_prev
-    
-    
-    if alpha_i > u:
-        is_new_proposal_accepted = 1
-        return theta_proposal, likelihood_proposal, is_new_proposal_accepted, freq_i
-    else:
-        is_new_proposal_accepted = 0
-        return theta_prev, nll_prev,is_new_proposal_accepted, None
-
-initial_theta = np.random.uniform(prior_lower_limit, prior_upper_limit,4)
-
-max_iter = 25_000
-
-
-nll_array = np.zeros(max_iter+1)
-theta_iter = np.zeros([len(true_params),max_iter+1])
-
-freq_list = []
-
-nll = unnormalized_posterior(initial_theta)
-
-
-theta_iter[:,0] = initial_theta
-nll_array[0],freq_i =  unnormalized_posterior(initial_theta)
-freq_list.append(freq_i)
-
-iteration = 1
-
-list_accepted_proposal = [1]
-n_accepted_proposals = 1
-
-
-
-
-#%% start or continue iterations
-
-
-while iteration < max_iter:
-    
-    theta_new,nll_new, accepted,freq_i = \
-        metropolis_sampling(theta_iter[:,iteration-1],
-                            nll_array[iteration-1])
-    theta_iter[:,iteration] = theta_new
-    nll_array[iteration] = nll_new
-    if freq_i is None: 
-        freq_list.append(freq_list[-1])
-    else:
-        freq_list.append(freq_i)
-    
-    
-    n_accepted_proposals += accepted
-    list_accepted_proposal.append(accepted)
-    iteration +=1
-    
-    if iteration%1000 == 0: print(iteration)
-    
-    if iteration%10000 == 0:
-        print("\n\n acceptance ratio ",n_accepted_proposals/iteration,
-              "\n\n")
-
-
-
-
-#%% plot inference
-
-burnin = 1000
-
-acceptance_vs_iter = np.array(list_accepted_proposal[burnin:])
-
-    
-
-
-fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,figsize=(20,10))
-fig.suptitle("m1", fontsize=16)
-ax1.hist(theta_iter[0,burnin:iteration],bins=100)
-ax1.set_title("histogram of samples")
-
-ax2.plot(theta_iter[0,burnin:iteration])
-# ax2.set_title("histogram of samples")
-
-ax4.plot(np.cumsum(acceptance_vs_iter)/(np.arange(len(acceptance_vs_iter))+1))
-plt.plot()
-
-
-fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,figsize=(20,10))
-fig.suptitle("m2", fontsize=16)
-ax1.hist(theta_iter[1,burnin:iteration],bins=100)
-ax1.set_title("histogram of samples")
-
-ax2.plot(theta_iter[1,burnin:iteration])
-# ax2.set_title("histogram of samples")
-
-ax4.plot(np.cumsum(acceptance_vs_iter)/(np.arange(len(acceptance_vs_iter))+1))
-plt.plot()
-
-
-
-
-
-figure = corner.corner(theta_iter[:2,burnin:iteration].T, 
-                        labels=["$m_1$", "$m_2$",], 
-                        show_titles=True)
-
-plt.show()
-
-
-
-# # true_params = [1.1,0.9,0.84,1.05]
-print(true_params)
-
-
-
-
-#%% plor frequencies of the samples from posterior and see if they match with the likelihood function
-
-
-freq_arr = np.array([list_i[0] for list_i in freq_list])
-MAC_arr = np.array([list_i[1] for list_i in freq_list])
-
-
-
-
-def plot_distribution3(distribution_function,params,ax,title="", limits_plot = [0.85,1.15]):
-    # Generate data for the Gaussian distribution
-    mu,sigma = params[0],params[1]
-    x = np.linspace(mu*limits_plot[0], mu*limits_plot[1], 1000)
-    y = distribution_function(x,*params)
-    # Plot the distribution
-    ax.plot(x, y, label=f'μ={mu:.2f}, σ={sigma:.2f}')
-    # ax.title(title)
-    # ax.xlabel('x')
-    # ax.ylabel('Probability Density')
-    ax.legend()
-    ax.grid(True)
-
-from scipy.stats import gaussian_kde
-
-samples = freq_arr[burnin:iteration,0]
-
-kde = gaussian_kde(samples)
-kde_x = np.linspace(min(samples), max(samples), 1000)
-kde_y = kde(kde_x)
-
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.hist(samples, bins=50, density=True, alpha=0.5, label='Histogram')
-ax.plot(kde_x, kde_y, 'r-', label='Kernel Density Estimate')
-ax.set_xlabel('Value')
-ax.set_ylabel('Density')
-ax.set_title('Kernel Density Estimate and Histogram - frequency 1')
-plot_distribution3(gaussian_distribution,[freq[0], sigma_f1],ax,"Frequency 1",[0.95,1.05])
-ax.legend()
-plt.show()
-
-
-
-samples = freq_arr[burnin:iteration,1]
-
-kde = gaussian_kde(samples)
-kde_x = np.linspace(min(samples), max(samples), 1000)
-kde_y = kde(kde_x)
-
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.hist(samples, bins=50, density=True, alpha=0.5, label='Histogram')
-ax.plot(kde_x, kde_y, 'r-', label='Kernel Density Estimate')
-ax.set_xlabel('Value')
-ax.set_ylabel('Density')
-ax.set_title('Kernel Density Estimate and Histogram - frequency 2')
-plot_distribution3(gaussian_distribution,[freq[1], sigma_f2],ax,"Frequency 2",[0.95,1.05])
-ax.legend()
-plt.show()
+    print("periods :",1/freq)
