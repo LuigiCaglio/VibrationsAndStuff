a7a0d96b824b01df91e445300381760ac25d77d2
LuigiCaglio <83336089+LuigiCaglio@users.noreply.github.com>
2024-11-23 00:21:19 +0100

    Update CB.jl

--- Julia_FEM/linearMOR.jl
+++ Julia_FEM/linearMOR.jl
@@ -37,24 +37,27 @@ end
 function CraigBamptonReduction(M::Matrix{Float64},C::Matrix{Float64},K::Matrix{Float64},
                     DOFs_substructure::Vector{Int},n_modes_substructure::Int)
     
+    # define retained DOFs of main structure
     DOFs_retained = setdiff(1:min(size(M)...), DOFs_substructure)
 
+    #number of DOFs and submatrix of stiffness      
+    nDOFs_full = size(M,1)
+    nDOFs_retained = length(DOFs_retained)
+    nDOFs_reduced = size(M,1)-length(DOFs_substructure)+n_modes_substructure
+    Ksm = K[DOFs_substructure,DOFs_retained]
+
+    # compute mode shapes ϕss of substructure
     Mss = M[DOFs_substructure,DOFs_substructure]
     Kss = K[DOFs_substructure,DOFs_substructure]
     ϕss = eigvecs(Kss,Mss)
 
-    Ksm = K[DOFs_substructure,DOFs_retained]
-
-    nDOFs_full = size(M,1)
-    nDOFs_retained = length(DOFs_retained)
-    nDOFs_reduced = size(M,1)-length(DOFs_substructure)+n_modes_substructure
-
-    #the dofs will be shuffled but it's ok
+    # compute transformation matrix (the dofs might be shuffled)
     T_CB = zeros(nDOFs_full,nDOFs_reduced)
     T_CB[DOFs_retained,1:nDOFs_retained] .= I(length(DOFs_retained))
     T_CB[DOFs_substructure,1:nDOFs_retained] .=  -Kss\Ksm
     T_CB[DOFs_substructure,nDOFs_retained+1:end] .= ϕss[:,1:n_modes_substructure]
     
+    # compute reduced matrices
     Mr, Cr, Kr = T_CB'*M*T_CB, T_CB'*C*T_CB, T_CB'*K*T_CB
     return Mr, Cr, Kr, T_CB
 end
